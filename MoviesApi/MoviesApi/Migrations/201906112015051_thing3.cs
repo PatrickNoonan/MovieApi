c9c954cf4174e0192f22843ae8e7004e8f5b9ffd
Patrick <Patricknnoonan@gmail.com>
2019-06-11 15:19:01 -0500

    Seeded database with movie model

---
+++ MoviesApi/MoviesApi/Migrations/201906112015051_thing3.cs
@@ -0,0 +1,28 @@
+namespace MoviesApi.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class thing3 : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Movies",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Title = c.String(),
+                        Genre = c.String(),
+                        DirectorName = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Movies");
+        }
+    }
+}
--- MoviesApi/MoviesApi/Models/Movies.cs
+++ MoviesApi/MoviesApi/Models/Movies.cs
@@ -8,19 +8,17 @@ namespace MoviesApi.Models
 {
     public class Movies
     {
-        public class Movie
-        {
-            [Key]
-            public int Id { get; set; }
+        [Key]
+        public int Id { get; set; }
 
-            [Display(Name = "Title")]
-            public string Title { get; set; }
+        [Display(Name = "Title")]
+        public string Title { get; set; }
 
-            [Display(Name = "Genre")]
-            public string Genre { get; set; }
+        [Display(Name = "Genre")]
+        public string Genre { get; set; }
 
-            [Display(Name = "Director Name")]
-            public string DirectorName { get; set; }
-        }
+        [Display(Name = "Director Name")]
+        public string DirectorName { get; set; }
+        
     }
 }
