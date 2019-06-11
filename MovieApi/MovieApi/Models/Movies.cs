07e22ae869fa2b7926ba367039925aef4c158c11
Patrick <Patricknnoonan@gmail.com>
2019-06-11 11:48:48 -0500

    Created a movie model with id, title, genre, and director properties

---
+++ MovieApi/MovieApi/Models/Movies.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MovieApi.Models
+{
+    public class Movies
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Display(Name = "Title")]
+        public string title { get; set; }
+
+        [Display(Name = "Genre")]
+        public string genre { get; set; }
+
+        [Display(Name = "Director Name"]
+
+        public string director { get; set; }
+    }
+}
