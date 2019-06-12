d35829036a6859f4cbacef030d89488ad469ac35
Patrick <Patricknnoonan@gmail.com>
2019-06-11 20:05:35 -0500

    Finished html to append to and css styling, with the help of itunes project.  Finished get method using ajax

--- MovieScripts.js
+++ MovieScripts.js
@@ -25,13 +25,16 @@
 $(document).ready(function () {
 
     $("button").on("click", function () {
-        let searchType = "song";
-        let searchInput = $("#user-search").val();
-        let inputToUrl = searchInput.replace(" ", "+");
+        let searchTitleInput = $("#user-search").val();
+
+        var movie = new Object();  
+        movie.Title = $('#title').val();  
+        movie.Genre = $('#genre').val();  
+        movie.DirectorName = $('#directorName').val(); 
 
         $.ajax({
             method: "GET",
-            url: "https://itunes.apple.com/search?entity=" + searchType + "&limit=8&term=" + inputToUrl,
+            url: "https://localhost:44378/api/values",
             dataType: "JSON"
         })
 
@@ -39,26 +42,26 @@ $(document).ready(function () {
 
                 console.log(data);
                 /*
-                let albumCover = data.results[0].artworkUrl100;
-                let artistName = data.results[0].artistName;
-                let songName = data.results[0].trackName;
-                let songPreview = data.results[0].previewUrl
+                let albumCover = data[0].artworkUrl100;
+                let artistName = data[0].artistName;
+                let songName = data[0].trackName;
+                let songPreview = data[0].previewUrl
                 */
-
-                console.log(data.results);
 
                 //$("#info-list").append(`<div class="col-2"><h3>Album Cover</h3></div><div class='col-2'><h3>Artist</h3></div><div class='col-2'><h3>Song</h3></div><div class='col-4'><h3>Album Name</h3></div><div class='col-2'><h3>Song Preview</h3></div>`);
 
-                $.each(data.results, function (key, value) {
+                $.each(data, function (key, value) {
 
                     $("#info-list")
-                        .append(`<div class="row object-row"><div class="col-2"><img src="${value.artworkUrl100}"></div>` + "<div class='col-2'>" + value.artistName + "</div><div class='col-2'>" + value.trackName + "</div><div class='col-4'>" + value.collectionName + "</div><div class='col-2'><audio controls='controls'><source src='" + value.previewUrl + `' type="audio/mpeg"></audio></div></div>`)
+                        // .append(`<div class="row object-row"><div class="col-2"><img src="${value.artworkUrl100}"></div>` + "<div class='col-2'>" + value.artistName + "</div><div class='col-2'>" + value.trackName + "</div><div class='col-4'>" + value.collectionName + "</div><div class='col-2'><audio controls='controls'><source src='" + value.previewUrl + `' type="audio/mpeg"></audio></div></div>`)
+                        // .append(`<div class="row object-row"><div class="col-2"><img src="${value.artworkUrl100}"></div>` + "<div class='col-2'>" + value.title + "</div><div class='col-2'>" + value.genre + "</div><div class='col-4'>" + value.DirectorName + "</div><div class='col-2'><audio controls='controls'><source src='" + value.previewUrl + `' type="audio/mpeg"></audio></div></div>`)
+                        .append(`<div class="row object-row"><div class="col-3">` + value.Id + `</div>` + "<div class='col-3'>" + value.Title + "</div><div class='col-3'>" + value.Genre + "</div><div class='col-3'>" + value.DirectorName + "</div>")
                 });
             })
 
             .fail(function () {
                 console.log("fail");
-                $(".iTunes-seach").append("<p>search failed, try again</p>")
+                $(".movieSearch").append("<p>search failed, try again</p>")
             });
 
     });
